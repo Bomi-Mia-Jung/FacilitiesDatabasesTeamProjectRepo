cf4317bacb9d9a8ca6388e4452e181250ca728c1
hailinkim <haikim20@amherst.edu>
2023-12-10 19:55:43 -0500

    Added SQL code for ENUMs

---
+++ sql/relational/utils.sql
@@ -0,0 +1,13 @@
+-- create ENUM
+create type location as enum ('Building Exterior', 'Lobby','Mechanical Room', 'Restrooms','Roof','Site','Throughout Building', 'Utility Closet');
+create type condition as enum ('Excellent', 'Good', 'Fair', 'Poor', 'Failed','Unknown');
+create type failureConsequence as enum ('Nuisance', 'Program Interruption', 'Partial Building Shutdown', 
+'Full Building Shutdown', 'Multi-Building Shutdown', 'Campus Shutdown', 'Fire/Lifesafety');
+create type class as enum ('Building', 'Court', 'Field', 'Road', 'Utility');
+create type purpose as enum ('Academic', 'Administrative', 'Athletic', 'Campus', 'Faculty Housing', 'Residence Hall', 'Student Life');
+
+-- delete ENUM
+drop type failureConsequence;
+
+--get ENUM values
+select * from pg_catalog.pg_enum ;
