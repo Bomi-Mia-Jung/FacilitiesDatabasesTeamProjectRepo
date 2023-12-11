0ce753a52b4a5310869c94088c1219cac058e881
hailinkim <haikim20@amherst.edu>
2023-12-10 21:46:34 -0500

    Added code for project cost viz

--- sql/viz/project_cost.sql
+++ sql/viz/project_cost.sql
@@ -3,13 +3,11 @@ select
 	p.asset_name as asset_name, asset_class, 
 	asset_type, street_addr, latitude, longitude, 
 --variables used for project costs
-	pid, project_name, consultestimate_high,
-	case when consultestimate_low is null 
-		then p.unit*p.unitcost*replacementfactor*softcostfactor*difficultyfactor  
-		else consultestimatelow end as consultestimatelow,  
-	complete, inprogress, actualcost
+	pid, project_name, consult_estimate_high,
+	case when consult_estimate_low is null 
+		then quantity*unit_cost*replacement_factor*soft_cost_factor*difficulty_factor  
+		else consult_estimate_low end as consult_estimate_low,  
+	complete, in_progress, actual_cost
 from projects p
-left join elements e 
-on p.eid = e.eid
 left join assets a
-on e.assetname = a.aname;
+on p.asset_name = a.asset_name;
